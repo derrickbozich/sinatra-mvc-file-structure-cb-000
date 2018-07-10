8d378df3c2b3517897734ff47498c34879ada094
derrick b <dbozich1@gmail.com>
2018-07-10 03:56:33 +0000

    automatically backed up by learn

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,15 +1,3 @@
 class Model
-  attr_accessor :name, :breed, :age
-
-  @@all = []
   
-  def initialize(name, breed, age)
-    @name = name
-    @breed = breed
-    @age = age
-  end
-
-  def self.all
-    @@all
-  end
 end
